b11f34a3d92729208399dd333c7a4666d287ddde
Nana5aki <lvxuanrui1@outlook.com>
2025-04-26 15:32:34 +0800

    feat: http-test clone 26/4/2025

---
+++ tests/test_http.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author: Nana5aki
+ * @Date: 2025-04-26 15:17:16
+ * @LastEditors: Nana5aki
+ * @LastEditTime: 2025-04-26 15:18:57
+ * @FilePath: /MySylar/tests/test_http.cpp
+ */
+#include "sylar/http/http_request.h"
+#include "sylar/http/http_response.h"
+#include <iostream>
+
+void test_request() {
+  sylar::http::HttpRequest::ptr req(new sylar::http::HttpRequest);
+  req->setHeader("host", "www.baidu.com");
+  req->setBody("testtest");
+  req->dump(std::cout) << std::endl;
+}
+
+void test_response() {
+  sylar::http::HttpResponse::ptr rsp(new sylar::http::HttpResponse);
+  rsp->setHeader("X-X", "TEST");
+  rsp->setBody("testtest");
+  rsp->setStatus((sylar::http::HttpStatus)400);
+  rsp->setClose(false);
+
+  rsp->dump(std::cout) << std::endl;
+}
+
+int main(int argc, char** argv) {
+  test_request();
+  test_response();
+  return 0;
+}
